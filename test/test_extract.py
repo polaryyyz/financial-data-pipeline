2ebba46e8d84cb0986f3da82843fd75ad2cd7454
polaryyyz <khfhbd65zf@privaterelay.appleid.com>
2026-08-26 15:30:28 +0200

    Finalizaing test for extract

--- test/test_extract.py
+++ test/test_extract.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch, MagicMock
 from src.extract import extract_company_info
 import src.extract 
+import requests
 
 def test_extract_company_info():
     with patch("src.extract.api_key", "test_api_key"):
@@ -24,3 +25,37 @@ def test_extract_company_info():
 
             assert data.iloc[0]["companyName"] == "Apple Inc."
             mock_get.assert_called_once_with(f"https://financialmodelingprep.com/stable/profile?symbol=AAPL", params={"apikey": "test_api_key"}, timeout=10)
+
+def test_extract_company_info_no_data():
+    with patch("src.extract.api_key", "test_api_key"):
+            with patch("requests.get") as mock_get:
+                mock_response = MagicMock()
+                mock_response.status_code = 200
+                mock_response.json.return_value = []
+    
+                mock_get.return_value = mock_response
+
+                data = extract_company_info("AAPL")
+
+                assert data is None
+
+def test_extract_company_info_http_error():
+    with patch("src.extract.api_key", "test_api_key"):
+        with patch("requests.get") as mock_get:
+            mock_response = MagicMock()
+            mock_response.raise_for_status = MagicMock(side_effect = requests.exceptions.HTTPError())
+
+            mock_get.return_value = mock_response
+
+            data = extract_company_info("AAPL")
+
+            assert data is None
+
+def test_extract_company_info_timeout_error():
+    with patch("src.extract.api_key", "test_api_key"):
+        with patch("requests.get") as mock_get:
+            mock_get.side_effect = requests.exceptions.Timeout
+
+            data = extract_company_info("AAPL")
+
+            assert data is None
